fef83763dbc4c107f80c929b672b7d3f6cdf6683
Bruno <brunohoffmann.2003@gmail.com>
2024-08-16 16:48:21 -0300

    Muda para arquivo main. Adiciona menu, função de executar jogada, imprimir tabuleiro e parte do sistema de dois jogadores

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+int tabuleiro[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
 void menu(){
     printf("=================================\n");
@@ -8,12 +11,57 @@ void menu(){
     printf("Escolha: ");
 }
 
+void imprime_tabuleiro(){
+    system("cls");
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            printf("%i  ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void jogada(int linha, int coluna, int jogador){
+    tabuleiro[linha - 1][coluna - 1] = jogador;
+}
+
+void pvp(){
+    int i = 1;
+    while(1){
+        imprime_tabuleiro();
+        int linha = 0;
+        int coluna = 0;
+        printf("Jogada (linha e coluna): ");
+        scanf("%i", &linha);    
+        scanf("%i", &coluna);
+
+        if(linha > 3 || linha < 1 || coluna > 3 || coluna < 1 || isalpha(linha) || isalpha(coluna) || tabuleiro[linha - 1] == 0 || tabuleiro[coluna - 1] == 0){
+            while(getchar() != '\n');
+            continue;
+        }
+
+        jogada(linha, coluna, (i % 2) + 1);
+        i += 1;
+    }
+}
+
+
 int main(){
-    
     int opcao = 0;
     do{
         menu();
         scanf("%i", &opcao);
+        while(getchar() != '\n');
+        system("cls");
+        if(opcao == 1){
+            pvp();
+        }
+        else if(opcao == 2){
+
+        }
+        else if(opcao != 3){
+            printf("Opcao invalida.\n");
+        }
 
     } while(opcao != 3);
 
