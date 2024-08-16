e3086058c9b264546265d4e7f62bd2c08fa642cc
Bruno <brunohoffmann.2003@gmail.com>
2024-08-16 17:20:32 -0300

    Adiciona início da implementação do sistema de vitória, apresentando problemas de lógica (jogadores ganhando na rodada 1)

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ void menu(){
 }
 
 void imprime_tabuleiro(){
-    system("cls");
+    // system("cls");
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
             printf("%i  ", tabuleiro[i][j]);
@@ -21,13 +21,28 @@ void imprime_tabuleiro(){
     }
 }
 
+int testa_vitoria(int linha, int coluna){
+    if(tabuleiro[linha][0] == (tabuleiro[linha][1] == tabuleiro[linha][2])){
+        printf("Linha\n");
+        return 1;
+    }
+    else if(tabuleiro[0][coluna] == (tabuleiro[1][coluna] == tabuleiro[2][coluna])){
+        printf("Coluna\n");
+        return 1;
+    }
+    else{
+        printf("Nao\n");
+        return 0;
+    }
+}
+
 void jogada(int linha, int coluna, int jogador){
-    tabuleiro[linha - 1][coluna - 1] = jogador;
+    tabuleiro[linha][coluna] = jogador;
 }
 
 void pvp(){
     int i = 1;
-    while(1){
+    while(i < 10){
         imprime_tabuleiro();
         int linha = 0;
         int coluna = 0;
@@ -40,7 +55,12 @@ void pvp(){
             continue;
         }
 
-        jogada(linha, coluna, (i % 2) + 1);
+        jogada(linha - 1, coluna - 1, (i % 2) + 1);
+        if(testa_vitoria(linha - 1, coluna - 1)){
+            imprime_tabuleiro();
+            printf("Jogador %i Venceu\n", (i % 2) + 1);
+            break;
+        }
         i += 1;
     }
 }
@@ -62,7 +82,6 @@ int main(){
         else if(opcao != 3){
             printf("Opcao invalida.\n");
         }
-
     } while(opcao != 3);
 
     return 0;
