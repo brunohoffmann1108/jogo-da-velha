b060d1d1f5b423d1b7f40a6b0aa0571374215e53
Bruno <brunohoffmann.2003@gmail.com>
2024-08-19 16:55:23 -0300

    Adiciona restante do código. Funcional mas lógica de vitória talvez possa ser melhorada

--- main.c
+++ main.c
@@ -1,38 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-int tabuleiro[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+#include <math.h>
+int tabuleiro[3][3];
+int jogador[3];
 
 void menu(){
-    printf("=================================\n");
-    printf("1. Jogador contra jogador\n");
-    printf("2. Jogador contra computador\n");
-    printf("3. Sair\n");
+    printf("1. Jogar novamente\n");
+    printf("2. Sair\n");
     printf("Escolha: ");
 }
 
-void imprime_tabuleiro(){
-    // system("cls");
+void zera_tabuleiro(){
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            printf("%i  ", tabuleiro[i][j]);
+            tabuleiro[i][j] = 0;
         }
-        printf("\n");
     }
 }
 
+void imprime_tabuleiro(){
+    system("cls");
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if(tabuleiro[i][j] == 1){
+                printf("  X  ");
+            }
+            else if(tabuleiro[i][j] == -2){
+                printf("  O  ");
+            }
+            else{
+                printf("     ");
+            }
+            if(j < 2){
+                printf("|");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n      %i x %i\n\n", jogador[1], jogador[0]);
+}
+
 int testa_vitoria(int linha, int coluna){
-    if(tabuleiro[linha][0] == (tabuleiro[linha][1] == tabuleiro[linha][2])){
-        printf("Linha\n");
+    if(tabuleiro[linha][0] == tabuleiro[linha][1] && tabuleiro[linha][1] == tabuleiro[linha][2]){
         return 1;
     }
-    else if(tabuleiro[0][coluna] == (tabuleiro[1][coluna] == tabuleiro[2][coluna])){
-        printf("Coluna\n");
+    else if(tabuleiro[0][coluna] == tabuleiro[1][coluna] && tabuleiro[1][coluna] == tabuleiro[2][coluna]){
         return 1;
+    }
+    else if((linha == coluna && linha == 1) && ((tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]) || (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]))){
+        return 1;
+    }
+    else if((linha == coluna && linha != 1) && (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2])){
+        return 1;
+    }   
+    else if((linha != coluna && linha != 1 && coluna != 1) && (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0])){
+        return 1;
+    }
+    return 0;
+}
+
+void recebe_jogada(int *linha, int *coluna){
+    *linha = 0;
+    *coluna = 0;
+    printf("Jogada (linha e coluna): ");
+    scanf("%i", linha);    
+    scanf("%i", coluna);
+
+}
+
+int valida_jogada(int linha, int coluna){
+    // Jogada com número inválido.
+    if(linha > 3 || linha < 1 || coluna > 3 || coluna < 1 || isalpha(linha) || isalpha(coluna)){
+        return 1;
+    }
+    // Jogada em posição já ocupada.
+    else if(tabuleiro[linha - 1][coluna - 1] != 0){
+        return 1;
+    }
+    return 0;
+}
+
+int define_jogador(int iterador){
+    if(iterador % 2 == 0){
+        return -2;
     }
     else{
-        printf("Nao\n");
-        return 0;
+        return 1;
     }
 }
 
@@ -40,49 +94,41 @@ void jogada(int linha, int coluna, int jogador){
     tabuleiro[linha][coluna] = jogador;
 }
 
-void pvp(){
+void jogo(){
+    int linha;
+    int coluna;
     int i = 1;
     while(i < 10){
         imprime_tabuleiro();
-        int linha = 0;
-        int coluna = 0;
-        printf("Jogada (linha e coluna): ");
-        scanf("%i", &linha);    
-        scanf("%i", &coluna);
-
-        if(linha > 3 || linha < 1 || coluna > 3 || coluna < 1 || isalpha(linha) || isalpha(coluna) || tabuleiro[linha - 1][coluna - 1] != 0){
+        recebe_jogada(&linha, &coluna);
+        if(valida_jogada(linha, coluna)){
             while(getchar() != '\n');
             continue;
         }
-
-        jogada(linha - 1, coluna - 1, (i % 2) + 1);
+        jogador[2] = define_jogador(i);
+        jogada(linha - 1, coluna - 1, jogador[2]);
         if(testa_vitoria(linha - 1, coluna - 1)){
+            jogador[i % 2] += 1;
             imprime_tabuleiro();
-            printf("Jogador %i Venceu\n", (i % 2) + 1);
+            printf("Jogador %i Venceu\n", abs(jogador[2]));
             break;
         }
         i += 1;
     }
 }
 
-
 int main(){
-    int opcao = 0;
+    int opcao;
     do{
-        menu();
-        scanf("%i", &opcao);
-        while(getchar() != '\n');
-        system("cls");
-        if(opcao == 1){
-            pvp();
+        opcao = 0;
+        zera_tabuleiro();
+        jogo();
+        while(opcao != 1 && opcao != 2){
+            menu();
+            scanf("%i", &opcao);
+            while(getchar() != '\n'); // Limpa buffer caso seja um caractere.
+            system("cls");
         }
-        else if(opcao == 2){
-
-        }
-        else if(opcao != 3){
-            printf("Opcao invalida.\n");
-        }
-    } while(opcao != 3);
-
+    } while(opcao != 2);
     return 0;
 }
